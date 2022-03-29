6fa58088c0d8305bf3b4ffb69748a7f152b31a3d
Nostress767 <72526817+Nostress767@users.noreply.github.com>
2022-03-29 15:47:59 +0900

    Fixed and renamed make.bat
    The idea is to make install_msvc.bat a click and play file.

--- install_msvc.bat
+++ install_msvc.bat
@@ -1,4 +1,3 @@
 curl -L --output vs_buildtools.exe --url https://aka.ms/vs/17/release/vs_buildtools.exe
 vs_buildtools.exe --quiet --add Microsoft.VisualStudio.Workload.VCTools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
 if exist vs_buildtools.exe del vs_buildtools.exe
